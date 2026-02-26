098c241de0fbb33738e8d522491b80ee92f7e1f7
Hyman Gao <gaohj1@lenovo.com>
2026-02-26 16:51:37 +0800

    Add root .gitignore and stop tracking generated files

--- Rent_Andriod_App/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_sublandlord_SubLandlordApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.sublandlord.SubLandlordApplication"
-)
-public final class _com_example_sublandlord_SubLandlordApplication {
-}
